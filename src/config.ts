a3b8648982dcc8dd286a1ecc1ed82c1c16065fd9
Sony AK <sony@sony-ak.com>
2023-06-29 13:02:20 +0700

    Initial code

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+import { name, version, description } from "../package.json";
+
+export const appName = name;
+export const appVersion = version;
+export const appDescription = description;
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import { chromium } from "playwright";
+import { cli } from "cleye";
+import { appName, appVersion } from "./config";
+
+async function main() {
+  const argv = cli({
+    name: appName,
+    version: appVersion,
+    parameters: [
+        '<host>'
+    ],
+    flags: {
+        scheme: {
+            type: String,
+            description: 'Scheme to use',
+            default: 'https'
+        } 
+    }
+  });
+
+  const url = `${argv.flags.scheme}://${argv._.host}`;
+
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(url);
+  await page.screenshot({ path: "example.png" });
+  await browser.close();
+}
+
+main();
